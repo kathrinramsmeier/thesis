132e7b6d7e1cc7e97b8fb766e386db66e4dd54ad
kathrinramsmeier <ramsmeierk@gmail.com>
2024-05-06 17:13:12 +0200

    minor changes

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -45,6 +45,12 @@ recordinginfo_header <- read.csv("C:\\Users\\ramsm\\Desktop\\Master\\Thesis\\dat
 colnames(recordinginfo) <- recordinginfo_header
 rm(list = "recordinginfo_header")
 
+# probe
+probe <- read.csv("C:\\Users\\ramsm\\Desktop\\Master\\Thesis\\data\\C190127-npy\\probe.csv", header = FALSE)
+probe_header <- read.csv("C:\\Users\\ramsm\\Desktop\\Master\\Thesis\\data\\C190127-npy\\probe_header.csv", header = FALSE)
+colnames(probe) <- probe_header
+rm(list = "probe_header")
+
 
 
 # Data inspection ---------------------------------------------------------
@@ -55,6 +61,8 @@ dim(LFP)
 summary(time) # 0 is stimulus, before ore-stimulus, after past-stimulus
 length(time) # 2 values less than LFP
 dim(behaviour)
+
+probe # layer 1:5 = upper, 6:10 = middle, 11:15 = deep
 
 
 
@@ -71,23 +79,27 @@ LFP <- LFP[, , trials_ind]
 task <- task[trials_ind, ] 
 task$trial_number_count <- 1:nrow(task)
 
-# # only investigate the ultimate time before the stimulus onset
-# time_ind <- which(time > -100)
+# # only investigate the LFP after stimulus onset and up to 600 ms after stimulus onset; clipping 10 ms after stimulus onset 
+# time_ind <- which(time > 10)
 # time <- time[time_ind]
 # LFP <- LFP[, time_ind, ]
 
-dim(LFP)
-dim(task)
-length(time)
+# clipping 10 ms before and after stimulus onset
+time_ind <- which(time > 10 | time < -10)
+time <- time[time_ind]
+LFP <- LFP[, time_ind, ]
 
 
 
-# Necessary variable ------------------------------------------------------
+# Checks ------------------------------------------------------------------
 
-# clipping the LFP
-time_clipped_ind <- which((time < -10) | (time > 10))
-time <- time[time_clipped_ind]
-LFP <- LFP[, time_clipped_ind, ]
+dim(LFP)[1] == 15
+dim(LFP)[2] == length(time)
+dim(LFP)[3] == nrow(task)
+
+
+
+# Necessary variables -----------------------------------------------------
 
 # filter all primed and unprimed trials (indices)
 primed_ind <- task$trial_number_count[task$block_trial_count %in% c(1, 2)]
@@ -109,8 +121,27 @@ frequency_bands <- list(
 
 
 
+# Data exploration --------------------------------------------------------
 
+# LFP of a sample trial over time for all cortical channels over time
+par(mfrow = c(4, 4))
+for (i in 1:dim(LFP)[1]) {
+  plot(time, LFP[i, , 100], type = "l", main = paste("LFP of a Sample Trial in Channel", i))
+}
+par(mfrow = c(1, 1))
 
+# LFP of a sample trial over time for all cortical channels over time in one plot
+bluePalette <- RColorBrewer::colorRampPalette(c("lightblue", "darkblue"))
+colours <- bluePalette(15)
+plot(time, LFP[1, , 100], type = "l", col = colours[1], main = "LFP of a Sample Trial in Different Channels")
+for (i in 2:dim(LFP)[1]) {
+  lines(time, LFP[i, , 100], col = colours[i])
+}
+legend("topright", legend = paste("Channel", 1:dim(LFP)[1]), col = colours, lty = 1, cex = 0.4)
 
-
-
+# LFP of a sample trial over time for 3 cortical channels over time in one plot
+colours = c("green", "blue", "violet")
+plot(time, LFP[1, , 100], type = "l", lwd = 2, col = colours[1], main = "LFP of a Sample Trial in 3 Different Channels (Lower, Middle and Upper")
+lines(time, LFP[8, , 100], lwd = 2, col = colours[2])
+lines(time, LFP[15, , 100], lwd = 2, col = colours[3])
+legend("bottomright", legend = paste("Channel", c(1, 8, 15)), col = colours, lty = 1, cex = 0.8)
